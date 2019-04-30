811ba01b39b3c827e4356aca6469d2f2c8b54394
Grace <gracejwh@gmail.com>
2019-04-30 14:11:23 -0400

    add eval function

--- train/train.py
+++ train/train.py
@@ -32,21 +32,35 @@ def train(model, iterator, optimizer, criterion):
     model.train()
 
     for batch in iterator:
-
         optimizer.zero_grad()
-
         predictions = model(batch.text).squeeze(1)
-
         loss = criterion(predictions, batch.label)
-
         acc = binary_accuracy(predictions, batch.label)
 
         loss.backward()
-
         optimizer.step()
 
         epoch_loss += loss.item()
         epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+def evaluate(model, iterator, criterion):
+
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.eval()
+
+    with torch.no_grad():
+
+        for batch in iterator:
+            predictions = model(batch.text).squeeze(1)
+            loss = criterion(predictions, batch.label)
+            acc = binary_accuracy(predictions, batch.label)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
 
     return epoch_loss / len(iterator), epoch_acc / len(iterator)
 
