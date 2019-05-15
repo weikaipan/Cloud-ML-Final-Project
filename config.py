a633c514c4d110f6e3be356bf515bb9d1c9ff047
weikaipan <weikai.pan1023@gmail.com>
2019-05-15 11:55:03 -0400

    local test

--- config.py
+++ config.py
@@ -1,9 +1,2 @@
-# import os
-# REDIS_URL = "redis://{host}:{port}/1".format(
-#     host=os.getenv('REDIS_HOST', 'localhost'),
-#     port=os.getenv('REDIS_PORT', '6379')
-# )
-# CELERY_BROKER_URL=REDIS_URL
-# CELERY_RESULT_BACKEND=REDIS_URL
-CELERY_BROKER_URL='redis://redis-service:6379/0'
-CELERY_RESULT_BACKEND='redis://redis-service:6379/0'
+CELERY_BROKER_URL='redis://localhost:6379/0'
+CELERY_RESULT_BACKEND='redis://localhost:6379/0'
